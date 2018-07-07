d8ea5e0dbd4ce29aa1d73777b93741b16f18d17c
Hansastreetlife <ionnaha@posteo.de>
2018-07-07 12:56:40 +0200

    Meine Django Girls App, erster Commit

---
+++ python_intro.py
@@ -0,0 +1,34 @@
+# print("1312 Hansastreetlife")
+# lottery=[46,37,17,3,45,2]
+# len(lottery)
+# print(lottery)
+# lottery.append(161)
+# print(lottery)
+# lottery.sort()
+# print(lottery)
+# lottery.reverse()
+# print(lottery)
+# a=12
+# b=11
+# b+a
+# a*b
+# a**b
+# print(a**b)
+# participant={"name":"Ionna", 'profession':"activist","numbers":[161,27,4]}
+# print(participant["profession"])
+# len(participant)
+# print(len(participant))
+# print(participant)
+# 6<2
+# print(6<2)
+# if 5>10:
+#     print("it works!")
+# else:
+#     print("hahaha")
+def hi(name):
+    print("hi "+ name +"!")
+
+people=["Ionna", "Ola", "Marie"]
+for name in people: 
+    hi(name)
+
